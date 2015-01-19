51725f003cd9b091c0f7dc6cbbd5b08ef271612b
Trung Vuong <trung.vuong@vndirect.com.vn>
2015-01-19 18:18:24 +0700

    reorganized routes

--- config/routes.js
+++ config/routes.js
@@ -46,6 +46,7 @@ module.exports.routes = {
   *                                                                          *
   ***************************************************************************/
 
+  'GET /user': 'UserController.index',
   'POST /user': 'UserController.create',
   'PUT /user/:id': 'UserController.update',
   'DELETE /user:id': 'UserController.destroy'
